d71c47122b3b51d0fe68910b9f37420a55d65d96
Nr1Dad <144018778+Nr1Dad@users.noreply.github.com>
2024-03-05 10:39:55 +0100

    Updated save, emils stuff

--- Assets/WorkShops/Save Emils stuff/Scripts/PlayerMovement.cs
+++ Assets/WorkShops/Save Emils stuff/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playermovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private BoxCollider2D coll;
@@ -82,7 +82,7 @@ public class playermovement : MonoBehaviour
         anim.SetInteger("state", (int)state);
     }*/
 
-   private bool IsGrounded()
+   public bool IsGrounded()
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
