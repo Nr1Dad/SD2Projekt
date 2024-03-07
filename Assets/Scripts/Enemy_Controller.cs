733d5db705aadad6b2293c85daf1cc38ee6c20d1
JohanneNissen <johanne@ornsovej.dk>
2024-03-07 19:24:12 +0100

    Enemy movement added
    Enemy can now move back and forth yay!

--- Assets/Scripts/Enemy_Controller.cs
+++ Assets/Scripts/Enemy_Controller.cs
@@ -6,6 +6,19 @@ using UnityEngine;
 public class Enemy_Controller : MonoBehaviour
 {
     public int enemyType;
+    private Rigidbody2D rb;
+    private Vector2 startPoint;
+    public Vector2 endPoint;
+    private Vector2 targetPoint;
+    public int speed = 1;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        startPoint = transform.position;
+        targetPoint = endPoint;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +42,27 @@ public class Enemy_Controller : MonoBehaviour
         
     }
 
-    public void OnTriggerEnter2D(Collider2D EnemyToPlayer)
+    private void FixedUpdate()
+    {
+        Vector2 currentPosition = transform.position;               
+
+        if (currentPosition == endPoint)
+        {
+            targetPoint = startPoint;
+            Debug.Log("Changed target to start point");
+        }
+        else if (currentPosition == startPoint)
+        {
+            targetPoint = endPoint;
+            Debug.Log("Changed target to end point");
+        }
+
+        Vector2 targetDirection = (targetPoint - currentPosition).normalized; 
+        rb.MovePosition(currentPosition + targetDirection * speed * Time.deltaTime);
+        Debug.Log("I'm moving");
+    }
+
+    /*public void OnTriggerEnter2D(Collider2D EnemyToPlayer)
     {
         
     }
@@ -38,8 +71,8 @@ public class Enemy_Controller : MonoBehaviour
     {
         if (EnemyToWall.gameObject.tag == "Wall")
         {
-
+            
         }
-    }
+    }*/
 }
 
